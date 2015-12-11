d63842ccbfa040df4b1fc3f7f1d5253a346c1018
amy <ayyshi@gmail.com>
2015-12-11 14:00:27 -0500

    completed migrations

---
+++ db/migrate/20151211164513_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string  :title
+      t.string  :description
+      t.boolean :completed, default: false
+      t.date    :date_completed
+
+      t.references :users
+
+      t.timestamps
+    end
+  end
+end
